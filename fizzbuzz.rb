a769b99ffe21091c6f626668718a64c2ff5b8542
hshockney <shockney@atlanticbb.net>
2019-06-13 09:27:44 -0400

    added sequence method to make tests pass

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,4 +2,13 @@ class Fizzbuzz
     def numbers
        (1..100).to_a 
     end
+    def sequence
+        numbers.collect do |number|
+            if (number % 3 == 0)
+                "Fizz"
+            else
+                number
+            end
+        end
+    end
 end
