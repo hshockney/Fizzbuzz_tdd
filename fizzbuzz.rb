4fb6aeee5e099116208e481f6a67bc323451fa8a
hshockney <shockney@atlanticbb.net>
2019-06-13 09:29:59 -0400

    Added to conditional to look for multiples of 5

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,6 +6,8 @@ class Fizzbuzz
         numbers.collect do |number|
             if (number % 3 == 0)
                 "Fizz"
+            elsif (number % 5 == 0)
+                "Buzz"
             else
                 number
             end
